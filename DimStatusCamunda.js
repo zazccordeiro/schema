c613e088ca1bae3fc7597b77e10b7cb60578630a
Isadora Gadelha Lima <isadora.lima@zazvendas.com.br>
2022-07-06 12:27:16 -0300

    adicionando arquivos na nova branch

---
+++ DimStatusCamunda.js
@@ -0,0 +1,63 @@
+cube(`DimStatusCamunda`, {
+  sql: `SELECT * FROM public.dim_status_camunda`,
+  
+  preAggregations: {
+    // Pre-Aggregations definitions go here
+    // Learn more here: https://cube.dev/docs/caching/pre-aggregations/getting-started  
+  },
+  
+  joins: {
+    
+  },
+  
+  measures: {
+    count: {
+      type: `count`,
+      drillMembers: [dateFrom, dateTo]
+    }
+  },
+  
+  dimensions: {
+    skDimStatusCamunda: {
+      sql: `sk_dim_status_camunda`,
+      type: `number`,
+      primaryKey: true
+    },
+    descricaoStatus: {
+      sql: `descricao_status`,
+      type: `string`
+    },
+    
+    variavel: {
+      sql: `variavel`,
+      type: `string`
+    },
+    
+    operacao: {
+      sql: `operacao`,
+      type: `string`
+    },
+    
+    depara: {
+      sql: `depara`,
+      type: `string`
+    },
+    
+    categoriaStatus: {
+      sql: `categoria_status`,
+      type: `string`
+    },
+    
+    dateFrom: {
+      sql: `date_from`,
+      type: `time`
+    },
+    
+    dateTo: {
+      sql: `date_to`,
+      type: `time`
+    }
+  },
+  
+  dataSource: `default`
+});
