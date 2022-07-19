0a8098f8f7f3658a2ddd3335559f3887f4ae7a23
Isadora Gadelha Lima <isadora.lima@zazvendas.com.br>
2022-07-19 15:28:41 -0300

    att

--- Conecta/Data.js
+++ Conecta/Data.js
@@ -1,5 +1,5 @@
 cube(`Data`, {
-  sql: `SELECT * FROM delivery.dim_data`,
+  sql: `SELECT * FROM public.dim_data`,
   
   preAggregations: {
     // Pre-Aggregations definitions go here
@@ -7,78 +7,168 @@ cube(`Data`, {
   },
   
   joins: {
-    
   },
   
   measures: {
-    count: {
+    quantidadeRegistros: {
       type: `count`,
-      drillMembers: [diaData]
+      drillMembers: [numeroAno],
+      shown: true,
     }
   },
   
   dimensions: {
-    fimSemana: {
-      sql: `fim_semana`,
-      type: `string`
+    skDimDataId: {
+      sql: `sk_dim_data_id`,
+      type: `number`,
+      primaryKey: true,
+    },
+
+    diaData: {
+      sql: `dia_data`,
+      type: `time`,
+      shown: true,
+    },
+
+    numeroAno: {
+      sql: `numero_ano`,
+      type: `number`,
+      shown: true,
+    },
+
+    numeroMes: {
+      sql: `nomero_mes`,
+      type: `number`,
+      shown: true,
+    },
+
+    numeroDiaAno: {
+      sql: `numero_dia_ano`,
+      type: `number`,
+      shown: true,
+    },
+
+    numeroDiaMes: {
+      sql: `numero_dia_mes`,
+      type: `number`,
+      shown: true,
     },
     
-    nomeDiaSemana: {
-      sql: `nome_dia_semana`,
-      type: `string`
+    numeroDiaSemana: {
+      sql: `numero_dia_semana`,
+      type: `number`,
+      shown: true,
+    },
+    
+    numeroSemanaAno: {
+      sql: `numero_semana_ano`,
+      type: `number`,
+      shown: true,
+    },
+    
+    numeroTrimestre: {
+      sql: `numero_trimestre`,
+      type: `number`,
+      shown: true,
+    },
+    
+    numeroSemanaMes: {
+      sql: `numero_semana_mes`,
+      type: `number`,
+      shown: true,
     },
     
     numeroTipoAno: {
       sql: `numero_tipo_ano`,
-      type: `string`
+      type: `number`,
+      shown: true,
     },
-    
-    nomesMes: {
+
+    nomeDiaSemana: {
+      sql: `nome_dia_semana`,
+      type: `string`,
+      shown: true,
+    },
+
+    nomeMes: {
       sql: `nomes_mes`,
-      type: `string`
+      type: `string`,
+      shown: true,
     },
-    
+
     nomeTrimestre: {
       sql: `nome_trimestre`,
-      type: `string`
+      type: `string`,
+      shown: true,
+    },
+
+    nomeTrimestreAno: {
+      sql: `nome_trimestre_ano`,
+      type: `string`,
+      shown: true,
+    },
+
+    nomeDia: {
+      sql: `nome_dia`,
+      type: `string`,
+      shown: true,
+    },
+
+    nomeSemana: {
+      sql: `nome_semana`,
+      type: `string`,
+      shown: true,
+    },
+
+    nomeSemanaMes: {
+      sql: `nome_semana_mes`,
+      type: `string`,
+      shown: true,
     },
     
     nomeTipoDiaSemana: {
       sql: `nome_tipo_dia_semana`,
-      type: `string`
+      type: `string`,
+      shown: true,
+    },
+
+    fimSemana: {
+      sql: `fim_semana`,
+      type: `string`,
+      shown: true,
     },
     
-    nomeSemana: {
-      sql: `nome_semana`,
-      type: `string`
-    },
-    
-    nomeTrimestreAno: {
-      sql: `nome_trimestre_ano`,
-      type: `string`
-    },
-    
-    nomeSemanaMes: {
-      sql: `nome_semana_mes`,
-      type: `string`
-    },
-    
-    nomeDia: {
-      sql: `nome_dia`,
-      type: `string`
-    },
-    
-    diaData: {
-      sql: `dia_data`,
-      type: `time`
-    },
-    skDimDataPedido: {
-      sql: `sk_dim_data_id`,
+    qtdDiasMes: {
+      sql: `qtd_dias_mes`,
       type: `number`,
-      primaryKey: true
-    }
+      shown: true,
+    },
+    
+    skDimSemanaId: {
+      sql: `sk_dim_semana_id`,
+      type: `number`,
+      shown: true,
+    },
+
+    skDimMesId: {
+      sql: `sk_dim_mes_id`,
+      type: `number`,
+      shown: false,
+    },
+    
+    skDimTrimestreId: {
+      sql: `sk_dim_trimestre_id`,
+      type: `number`,
+      shown: false,
+    },
+    
+    skDimAnoId: {
+      sql: `sk_dim_ano_id`,
+      type: `number`,
+      shown: false,
+    },
+    
   },
-  
   
   dataSource: `default`
 });
